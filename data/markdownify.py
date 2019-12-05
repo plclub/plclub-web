80d66efe51645891e2e396af4e30b3c067e642ee
euisuny <ey222@cornell.edu>
2019-12-04 20:08:37 -0500

    Uncommenting something..

--- data/markdownify.py
+++ data/markdownify.py
@@ -1,19 +1,19 @@
 import csv 
 
-# curr = ['student', 'faculty', 'postdoc']
-# for group in curr: 
-#     with open(group + '.csv') as csvfile: 
-#         reader = csv.reader(csvfile, delimiter=',')
-#         for row in reader: 
-#             ident = row[0].split(" ")[0] + row[0].split(" ")[1][0]
-#             f = open("../people/" + ident + ".md","w+")
-#             f.write("---\n")
-#             f.write("name : \"" + row[0].strip() + "\"\n")
-#             f.write("website : \"" + row[1].strip() + "\"\n")
-#             f.write("email : \"" + row[2].strip() + "@seas.upenn.edu\"\n")
-#             f.write("tags : [\"" + group + "\"]\n")
-#             f.write("headshot : \"" + ident + ".jpg\"\n")
-#             f.write("---\n")
+curr = ['student', 'faculty', 'postdoc']
+for group in curr: 
+    with open(group + '.csv') as csvfile: 
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader: 
+            ident = row[0].split(" ")[0] + row[0].split(" ")[1][0]
+            f = open("../people/" + ident + ".md","w+")
+            f.write("---\n")
+            f.write("name : \"" + row[0].strip() + "\"\n")
+            f.write("website : \"" + row[1].strip() + "\"\n")
+            f.write("email : \"" + row[2].strip() + "@seas.upenn.edu\"\n")
+            f.write("tags : [\"" + group + "\"]\n")
+            f.write("headshot : \"" + ident + ".jpg\"\n")
+            f.write("---\n")
 
 with open('alum.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
