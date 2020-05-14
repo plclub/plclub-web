7b7e2b25b2261b8859d91a2fc2c7db518990e49c
Lawrence Dunn <lawrence@lawrencedunn.io>
2020-05-14 15:09:21 -0400

    Move to git-based deployment system

--- extra/deploy.sh
+++ extra/deploy.sh
@@ -5,13 +5,13 @@ OUTPUT_DIR=_site/
 PLCLUB_ACCOUNT=plclub@eniac
 # Location of folder relative to remote user's home directory
 # This variable may also need to be set in ./deploy_remote.sh
-REMOTE_FOLDER=website_repo_test
-# Script to run on remote e.g. to make a tar backup before syncing
+REMOTE_FOLDER=html/
+# Script to run on remote e.g. to make a backup before syncing
 # this should be relative to the top-level repo folder
 REMOTE_SCRIPT=./extra/deploy_remote.sh
 
-# Run the remote helper script
-ssh plclub@eniac "bash -s" < $REMOTE_SCRIPT
-
 # Synchronize our local folder to the remote
-rsync -vr $OUTPUT_DIR plclub@eniac:$REMOTE_FOLDER
+rsync -vr $OUTPUT_DIR $PLCLUB_ACCOUNT:$REMOTE_FOLDER
+
+# Run the remote helper script
+ssh $PLCLUB_ACCOUNT "bash -s" < $REMOTE_SCRIPT
--- extra/deploy_remote.sh
+++ extra/deploy_remote.sh
@@ -1,4 +1,6 @@
 # This script is run on the remote (plclub@eniac)
 # it is launched by ./deploy.sh
-REMOTE_FOLDER=website_repo_test
-tar -zc -f $REMOTE_FOLDER-$(date '+%F').tar.gz $REMOTE_FOLDER
+REMOTE_FOLDER=html/
+cd $REMOTE_FOLDER
+git add .
+git commit -m "Automated commit"
