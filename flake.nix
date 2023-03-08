163d265dd54dc178cdcdc7703e526c1a9117e754
Lawrence Dunn <dunnla@seas.upenn.edu>
2023-03-08 00:17:31 -0500

    Update flake.nix to include Alectryon dependencies

--- flake.nix
+++ flake.nix
@@ -6,8 +6,9 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default =
       with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        nativeBuildInputs = [ ghc cabal-install zlib pkgconfig bibtex2html ];
+      let alectryon-deps = with python310Packages; [ coq coqPackages.serapi alectryon myst-parser ];
+      in stdenv.mkDerivation {
+        nativeBuildInputs = [ ghc cabal-install zlib pkgconfig bibtex2html ] ++ alectryon-deps;
         name = "hello";
         src = self;
         buildPhase = "cabal build";
